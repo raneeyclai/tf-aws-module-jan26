b3a2b58d2ecde45d812a40c14738725236333310
Ranee Bofa Jan <user9@ip-172-31-18-53.ap-south-1.compute.internal>
2026-01-21 09:27:34 +0000

    Updated output

--- module/security_groups/sg_output.tf
+++ module/security_groups/sg_output.tf
@@ -1,9 +1,11 @@
 output "web_sg_id" {
   description = "ID of the web security group"
   value       = aws_security_group.web_sg.id
+
+  vpc_security_group_ids = var.web_sg
 }
  
  
-    vpc_security_group_ids = var.web_sg
+
  
  
