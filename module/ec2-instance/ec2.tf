c922faf808fcc0a791303c8381b83612f220c358
Ranee Bofa Jan <user9@ip-172-31-18-53.ap-south-1.compute.internal>
2026-01-21 09:31:08 +0000

    Update ect2.tf

--- module/ec2-instance/ec2.tf
+++ module/ec2-instance/ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "main" {
     ami = var.region_ami_map[var.region-1]
     instance_type = var.instance_type
     subnet_id = var.subnets[count.index]
+    vpc_security_group_ids = var.web_sg
     count = var.num_of_vm
     associate_public_ip_address = false
    
